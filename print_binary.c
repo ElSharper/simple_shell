1895048d1070f2bbbdf2b45b06fa51c9438005a8
El Mehdi Bayoud <118342478+ElSharper@users.noreply.github.com>
2023-07-26 23:29:10 +0100

    Update print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_binary - displays the binary representation
+ * print_binaryy - displays the binary representation
  * of a number.
  * @n: unsigned long int.
  *
  * Return: void.
  */
-void print_binary(unsigned long int n)
+void print_binaryy(unsigned long int no)
 {
-	if (n >> 0)
+	if (no >> 0)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		if (no >> 1)
+			print_binaryy(no >> 1);
+		_putcharr((no & 1) + '0');
 	}
 	else
 	{
-		_putchar('0');
+		_putcharr('0');
 	}
 }
